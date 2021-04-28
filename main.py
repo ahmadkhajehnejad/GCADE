bug in use_bfs_incremental_par_idx fixed, MADE implemented, normalization layers removed except for PositionwiseFeedForward, graph_attention is changed to 2 layer MLP instead of a linear combination.

main.py
@@ -18,7 +18,7 @@ import pickle
 
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 # if __name__ == '__main__':
 
@@ -186,7 +186,6 @@ def cal_performance(pred, gold, trg_pad_idx, args, smoothing=False):
 
 def cal_loss(pred, gold, trg_pad_idx, args, smoothing=False):
     ''' Calculate cross entropy loss, apply label smoothing if needed. '''
-
     if smoothing:
         if args.input_type == 'node_based':
             gold = gold.contiguous().view(-1)
@@ -337,7 +336,7 @@ def generate_graph(gg_model, args):
         for i in range(args.max_seq_len - 1):
             if args.use_bfs_incremental_parent_idx:
                 min_par_idx = torch.zeros(src_seq.size(0), src_seq.size(2), dtype=torch.int32).bool().to(args.device)
-            pred_probs = torch.sigmoid(gg_model(src_seq, src_seq, adj)).view(-1, args.max_seq_len, args.max_num_node + 1)
+            pred_probs = torch.sigmoid(gg_model(src_seq, src_seq, None, adj)).view(-1, args.max_seq_len, args.max_num_node + 1)
             # if args.use_max_prev_node and i > args.max_prev_node:
             #     pred_probs[:, i, 1:i - args.max_prev_node + 1] = 0
             # if args.use_bfs_incremental_parent_idx:
@@ -364,7 +363,7 @@ def generate_graph(gg_model, args):
                 remainder_idx = remainder_idx & ((src_seq[:, i + 1, : i + 1] == args.one_input).sum(-1) == 0)
             new_finished_idx = not_finished_idx & (src_seq[:, i + 1, 0] == args.one_input)
             src_seq[new_finished_idx, i + 1, 1:] = args.src_pad_idx
-            if i > 0:
+            if i > 0 and args.use_bfs_incremental_parent_idx:
                 tmp = src_seq[not_finished_idx, i + 1, :] == args.one_input
                 min_par_idx[not_finished_idx, :] = tmp.cumsum(dim=1) == 0
                 min_par_idx[not_finished_idx, 0] = False
@@ -454,8 +453,8 @@ def train(gg_model, dataset_train, dataset_validation, optimizer, args):
             trsz += src_seq.size(0)
 
         val_running_loss = 0.0
-        vlsz = 1
-        '''
+        # vlsz = 1
+        # '''
         vlsz = 0
         gg_model.eval()
         for i, data in enumerate(dataset_validation, 0):
@@ -471,7 +470,7 @@ def train(gg_model, dataset_train, dataset_validation, optimizer, args):
 
             val_running_loss += loss.item()
             vlsz += src_seq.size(0)
-        '''
+        # '''
 
 
         loss_buffer.append(running_loss / trsz)

transformer/Models.py
@@ -89,6 +89,8 @@ class Encoder(nn.Module):
                 else:
                     sz_input_vec = n_ensemble * (args.max_prev_node + 1)
                     sz_emb = n_ensemble * d_word_vec
+            if args.input_bfs_depth:
+                sz_input_vec = sz_input_vec + args.max_num_node
             # self.src_word_emb = nn.Linear(sz_input_vec, sz_emb, bias=False)
             sz_intermed = max(sz_input_vec, sz_emb)
             self.src_word_emb_1 = nn.Linear(sz_input_vec, sz_intermed, bias=True)
@@ -103,7 +105,7 @@ class Encoder(nn.Module):
         self.layer_stack = nn.ModuleList([
             EncoderLayer(d_model, d_inner, n_ensemble, n_head, d_k, d_v, k_gr_att=args.k_graph_attention, dropout=dropout)
             for _ in range(n_layers)])
-        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+        # self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
         self.scale_emb = scale_emb
         self.d_model = d_model
 
@@ -126,7 +128,7 @@ class Encoder(nn.Module):
         if self.scale_emb:
             enc_output *= self.d_model ** 0.5
         enc_output = self.dropout(self.position_enc(enc_output))
-        enc_output = self.layer_norm(enc_output)
+        # enc_output = self.layer_norm(enc_output)
 
         for enc_layer in self.layer_stack:
             enc_output, enc_slf_attn = enc_layer(enc_output, slf_attn_mask=src_mask, gr_mask=gr_mask)
@@ -176,7 +178,7 @@ class Decoder(nn.Module):
         self.layer_stack = nn.ModuleList([
             DecoderLayer(d_model, d_inner, n_ensemble, n_head, d_k, d_v, k_gr_att=args.k_graph_attention, dropout=dropout)
             for _ in range(n_layers)])
-        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+        # self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
         self.scale_emb = scale_emb
         self.d_model = d_model
 
@@ -199,7 +201,7 @@ class Decoder(nn.Module):
         if self.scale_emb:
             dec_output *= self.d_model ** 0.5
         dec_output = self.dropout(self.position_enc(dec_output))
-        dec_output = self.layer_norm(dec_output)
+        # dec_output = self.layer_norm(dec_output)
 
         for dec_layer in self.layer_stack:
             dec_output, dec_slf_attn, dec_enc_attn = dec_layer(

transformer/SubLayers.py
@@ -75,8 +75,12 @@ class EnsembleMultiHeadAttention(nn.Module):
         self.n_ensemble_k = n_ensemble_k
 
         if k_gr_att > 0:
-            self.gr_att_linear_list = nn.ModuleList([
-                nn.Linear(k_gr_att, 1, bias=False)
+            self.gr_att_linear_list_1 = nn.ModuleList([
+                nn.Linear(k_gr_att, k_gr_att, bias=True)
+                for _ in range(n_ensemble_q * n_ensemble_k * n_head)
+            ])
+            self.gr_att_linear_list_2 = nn.ModuleList([
+                nn.Linear(k_gr_att, 1, bias=True)
                 for _ in range(n_ensemble_q * n_ensemble_k * n_head)
             ])
 
@@ -102,7 +106,7 @@ class EnsembleMultiHeadAttention(nn.Module):
         # self.attention = EnsembleScaledDotProductAttention(temperature=d_k ** 0.5)
 
         self.dropout = nn.Dropout(dropout)
-        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+        # self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
 
 
     def forward(self, q, k, v, mask=None, gr_mask=None):
@@ -155,16 +159,21 @@ class EnsembleMultiHeadAttention(nn.Module):
 
                 ################### ScaledDotProductAttention ######################
                 attn_ = torch.matmul(q_ens / self.temperature, k_ens.transpose(2, 3))
+                '''
+                attn_ = torch.zeros(sz_b, n_head, len_q, len_k).to(v.device)
+                '''
 
                 if mask is not None:
                     if gr_mask is not None:
                         for h in range(self.n_head):
-                            gr_att_linear = self.gr_att_linear_list[i * n_ensemble_k * n_head + j * n_head + h]
-                            gr_mask_agg = gr_att_linear(gr_mask.transpose(1,2).transpose(2,3))   #  sz_b * k_gr_attr * len_k * len_q ---> sz_b * 1 * len_k * len_q
+                            gr_att_linear_1 = self.gr_att_linear_list_1[i * n_ensemble_k * n_head + j * n_head + h]
+                            gr_att_linear_2 = self.gr_att_linear_list_2[i * n_ensemble_k * n_head + j * n_head + h]
+                            gr_mask_agg = gr_att_linear_2(F.relu(gr_att_linear_1(gr_mask.transpose(1,2).transpose(2,3))))   #  sz_b * k_gr_attr * len_k * len_q ---> sz_b * 1 * len_k * len_q
                             gr_mask_agg = gr_mask_agg.squeeze(-1)
                             attn_[:,h,:,:] = attn_[:,h,:,:] + gr_mask_agg
                             # attn_[:,h,:,:] = attn_[:,h,:,:] * gr_mask_agg
                             # attn_[:,h,:,:] = gr_mask_agg
+                            # attn_[:,h,:,:] = attn_[:,h,:,:].masked_fill(gr_mask_agg == 0, -1e9)
                             # attn_[:,h,:,:] = 1
                     attn_ = attn_.masked_fill(mask == 0, -1e9)
                 attn[:,:,:,:,j] = attn_
@@ -192,7 +201,7 @@ class EnsembleMultiHeadAttention(nn.Module):
         # if residual.size(2) == 1:
         #     residual = residual.repeat(1,1,n_ensemble_q,1)
         output += residual
-        output = self.layer_norm(output)
+        # output = self.layer_norm(output)
 
         return output, None # returns None for attn
 